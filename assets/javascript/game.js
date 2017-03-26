84cc31448d23098cb0a026de5ed700cb4fd323dc
Ajoy Kumar <Ajoy Kumar>
2017-03-25 22:11:13 -0500

    working no animations

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,27 @@
 //script for psychic game//
 var numWins=0;
 var numLosses=0;
-var wordDisplay="-"
-var guessesLeft=9;
+var wordDisplay=[];
+var guessesLeft=15;
 var yourGuesses=[];
 var userGuess="";
 var userWord="";
-var message="";
 //print initial variables//
 var words=["first", "second", "third", "fourth", "fifth"];
 
 var randomNumber=0;
 var computerChoice="";
+var message="";
 
 var displayResults=function(){
-	console.log(wordDisplay);
+	// console.log(wordDisplay);
 	document.getElementById("wins").innerHTML="Wins: "+ numWins;
 	document.getElementById("losses").innerHTML="Losses: "+ numLosses;
 	document.getElementById("currentWord").innerHTML="Current Word: "+ wordDisplay;
 	document.getElementById("guessesRemaining").innerHTML="Guesses Remaining: "+ guessesLeft;
 	document.getElementById("yourGuesses").innerHTML="Letters Already Guessed: "+ yourGuesses;
-	document.getElementById("banner").innerHTML=message +"! Press a letter to play again";
+	document.getElementById("banner").innerHTML= "Message:" + message;
+	return;
 }
 
 //generate a computer guessed letter//
@@ -28,41 +29,43 @@ computerGuess=function(){
 	randomNumber=Math.floor(Math.random()*5);
 	computerChoice=words[randomNumber];
 	console.log("computer choice " + computerChoice);
-	return(computerChoice);
+	return;
 }
 
 //display computer choice with "_"
-function displayBlanks(){
+function displayWord(){
 	var wordLength=computerChoice.length; 
 	wordDisplay=[];
  	for (var i=0; i<wordLength; i++){
  		wordDisplay.push("_");
  	}
- 	return (wordDisplay);
+ 	return;
 }
 
-//function to reset values
+//reset//function to reset values after one round play win or loss//
 function reset(){
 	yourGuesses=[];
-	guessesLeft=9;
+	guessesLeft=15;
 	computerGuess();
-	displayBlanks();
-	displayResults();
+	displayWord();
+	message="";
+	// displayResults();
+	return;
 }
- 	
-//display initial variables//
+
+//Main program.  Here is the main program//
+
+//display initial values for game//
 computerGuess();
-displayBlanks();
+displayWord();
 displayResults();
 
 //get user input letter//
 document.onkeydown=function(event){
 	userGuess=event.key;
-	yourGuesses.push(userGuess);
 			
 	//check if the letter is in word//
 	for (char in computerChoice){
-
 		if (computerChoice[char] == userGuess){
 	//add letters guessed by user in the right location//
 			wordDisplay[char]=userGuess;
@@ -71,24 +74,34 @@ document.onkeydown=function(event){
 	//convert the userword from array to a string and remove ","//
 	var tempWord=wordDisplay.join();
 	userWord=tempWord.replace(/\,/g,"");
-	// console.log("updated display "+ wordDisplay);
-	guessesLeft--;
-	displayResults();		
 
+	// check if the letter is pressed already; if not reduce guesses remaining and update display
+	var alreadyPressed=false;
+	var myArray = yourGuesses;
+	var mySet = new Set(myArray);
+	var alreadyPressed = mySet.has(userGuess); // true
+	if (!alreadyPressed){
+		yourGuesses.push(userGuess);
+		guessesLeft--;
+		displayResults();	
+	};
+
+	console.log(alreadyPressed)
+
+//when all letters are guessed display message and updated results//
 	if(userWord==computerChoice){
-		displayResults();
-		alert("You Win! The correct word is "+ wordDisplay +". Guessing new word!");
-		numWins++;
-		message="You Win";
-		reset();
+		// alert("You Win! The correct word is "+ wordDisplay +". Guessing new word!");
+		 numWins++;
+		 message="You Win! Press a letter to play again";
+		 displayResults();
+		 reset();
 	}
-
+// 15 guesses are exceeded //
 	if(guessesLeft<1){
 		numLosses++;
+		// alert("You lost.  The correct word is '"+ computerChoice +"'. Guessing new word.");
+		message="You Lost! Press a letter to play again";
 		displayResults();
-		alert("You lost.  The correct word is '"+ computerChoice +"'. Guessing new word.");
-		message="You Lost";
 		reset();
 	}
-
 }
